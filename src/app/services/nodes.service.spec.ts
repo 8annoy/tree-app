389b4afbe3456bd892001a887ad0637b292e60a9
8annoy <eitannoy@gmail.com>
2017-07-18 20:26:49 +0300

    cleanup

--- src/app/services/nodes.service.spec.ts
+++ src/app/services/nodes.service.spec.ts
@@ -1,35 +1,3 @@
-// import { TestBed, inject } from '@angular/core/testing';
-// import { MockBackend } from '@angular/http/testing';
-// import { Http, BaseRequestOptions } from '@angular/http';
-// import { NodesService } from './nodes.service';
-// import { Store } from '@ngrx/store';
-// import { MockStore } from './mock.store';
-
-// describe('NodesService', () => {
-//   beforeEach(() => {
-//     TestBed.configureTestingModule({
-//       providers: [
-//         NodesService, 
-//         MockBackend,
-//         {provide:Store, useValue: new MockStore({tags: []})},
-//         BaseRequestOptions,
-//         {
-//           provide: Http,
-//           useFactory: (backendInstance: MockBackend, defaultOptions: BaseRequestOptions) => {
-//             return new Http(backendInstance, defaultOptions);
-//           },
-//           deps: [MockBackend, BaseRequestOptions]
-//         }
-//       ]
-//     }).compileComponents();
-//   });
-
-//   it('should ...', inject([NodesService], (service: NodesService) => {
-//     // spyOn(Http, "get").and.returnValue([]);
-//     // expect(service.getNodes()).toBe("---");
-//   }));
-// });
-
 import { TestBed, inject } from '@angular/core/testing';
 import { Http, BaseRequestOptions, ResponseOptions, Response, Request, RequestMethod } from '@angular/http';
 import { MockBackend, MockConnection } from '@angular/http/testing';
